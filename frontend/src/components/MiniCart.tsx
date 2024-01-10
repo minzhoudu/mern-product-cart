e3cb088008e0f604f83ff98cac0c75a475da1ab8
Pavle <jovanovix.p@gmail.com>
2024-01-10 10:32:57 +0100

    [Fe: feature] navbar and minicart icon

---
+++ frontend/src/components/MiniCart.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface CartProps {}
+
+const Cart: FC<CartProps> = () => {
+    return <>Cart</>;
+};
+
+export default Cart;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 interface NavbarProps {}
 
@@ -7,15 +8,19 @@ const Navbar: FC<NavbarProps> = () => {
     return (
         <div className="w-full bg-orange-700 shadow-[0px_4px_10px_1px_rgba(0,0,0,0.75)]">
             <div className="flex justify-between items-center text-center mx-10 h-16">
-                <NavLink to="/" className="w-20">
-                    Home
+                <NavLink to="/" className="px-5 font-bold text-xl hover:text-white">
+                    HOME
                 </NavLink>
 
-                <input type="text" placeholder="search..." className="rounded-md h-3/5 w-56 px-5" />
+                <input type="text" placeholder="search..." className="rounded-md h-3/5 w-56 px-3" />
 
-                <div className="w-20">
-                    <h1>CartIcon</h1>
-                </div>
+                <button className="relative p-3 border-2 border-black rounded-lg text-2xl cursor-pointer hover:border-white hover:text-white">
+                    <FaShoppingCart />
+
+                    <div className="absolute bottom-0 left-0 w-2 h-2 rounded-full bg-white flex justify-center items-center p-2 text-orange-700 text-xs font-bold">
+                        3
+                    </div>
+                </button>
             </div>
         </div>
     );
